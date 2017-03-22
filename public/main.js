05801c8a10a8f4941d03616e74b0939d4483cb38
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-03-22 14:26:35 -0400

    reverted to original functionality

--- public/main.js
+++ public/main.js
@@ -1,25 +1,25 @@
-// function getPhotos(photosData) {
-//     for (var i in photosData) {
-//         var photo = photosData[i];
-//         var elem = $("<img>");
-//         elem.attr("src", photo.filename);
-//         $("#photos").append(elem);
-//     }
-// }
-// $.get("/photos", getPhotos);
-
 function getPhotos(photosData) {
     for (var i in photosData) {
-        // var photo = photosData[i];
-        var elem = $("<a>");
-        elem.attr("href", "photos/" + photosData[i].filename);
-        elem.text(photosData[i].filename);
+        var photo = photosData[i];
+        var elem = $("<img>");
+        elem.attr("src", photo.filename);
         $("#photos").append(elem);
-        var elem2 = $("<br>");
-        $("#photos").append(elem2);
     }
 }
 $.get("/photos", getPhotos);
+
+// function getPhotos(photosData) {
+//     for (var i in photosData) {
+//         // var photo = photosData[i];
+//         var elem = $("<a>");
+//         elem.attr("href", "photos/" + photosData[i].filename);
+//         elem.text(photosData[i].filename);
+//         $("#photos").append(elem);
+//         var elem2 = $("<br>");
+//         $("#photos").append(elem2);
+//     }
+// }
+// $.get("/photos", getPhotos);
 
 function getUser(userData) {
     if(userData.length == 0) {
